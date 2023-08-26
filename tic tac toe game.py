c69a7c7b6f8166d5878b2bc2f40794746fa7f301
mahesh8179 <114736621+mahesh8179@users.noreply.github.com>
2023-08-26 10:27:21 +0530

    Add files via upload

---
+++ tic tac toe game.py
@@ -0,0 +1,43 @@
+print("k.mahesh 192110349")
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    for col in range(3):
+        if all(row[col] == player for row in board):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def play_game():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+    
+    while True:
+        print_board(board)
+        row = int(input(f"Player {current_player}, enter row (0-2): "))
+        col = int(input(f"Player {current_player}, enter col (0-2): "))
+        
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a draw!")
+                break
+            current_player = 'O' if current_player == 'X' else 'X'
+        else:
+            print("Cell already taken. Try again.")
+
+play_game()
